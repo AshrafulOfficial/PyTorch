04410cd29e1efe43317c04f28239b66d966a37b3
ashraful <ashrafulofficialc@gmail.com>
2026-04-19 20:32:20 +0600

    ok

--- create_torch.py
+++ create_torch.py
@@ -1,6 +1,23 @@
 import torch
 
 #crete torch
-x = torch.tensor([1, 2, 3, 4, 5])
 
+#tensor is a container of numbers of ML
+
+#scalar
+x = torch.tensor(5)
 print(x)
+
+#vector
+y = torch.tensor([1, 2, 3])
+print(y)
+
+#matrix
+z = torch.tensor([[1, 2], [3, 4]])
+print(z)
+
+print(y.shape)
+print(z.shape)
+
+#size 3 means: 3 elements
+#size 2 2 means: 2 rows, 2 columns
