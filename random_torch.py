f37a21bed8317a3b42b950981ad7f5e01f3e893e
ashraful <ashrafulofficialc@gmail.com>
2026-04-19 20:42:16 +0600

    ok

---
+++ random_torch.py
@@ -0,0 +1,9 @@
+#the random functions takes random value
+#between 0 or more to less 1
+#in every time the value will change
+#2, 3 means: 2 rows, 3 columns
+
+import torch
+
+t = torch.rand(2,3)
+print(t)
